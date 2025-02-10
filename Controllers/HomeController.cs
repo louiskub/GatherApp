9f9cc86e6426932cdfbfa3a1953e1a8a33ee1d19
Manatsavin Kunnantajiam <louismnsv@gmail.com>
2025-02-10 17:44:25 +0700

    เพิ่มหน้า front end

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -18,6 +18,42 @@ public class HomeController : Controller
         return View();
     }
 
+    [Route("aboutus")]
+    public IActionResult AboutUs()
+    {
+        return View("~/Views/Home/AboutUs.cshtml");
+    }
+
+    [Route("history")]
+    public IActionResult History()
+    {
+        return View("~/Views/Home/History.cshtml");
+    }
+
+    [Route("post")]
+    public IActionResult ViewPost()
+    {
+        return View("~/Views/Home/Viewpost.cshtml");
+    }
+
+    [Route("profile")]
+    public IActionResult ViewProfile()
+    {
+        return View("~/Views/Home/Profile.cshtml");
+    }
+
+    [Route("login")]
+    public IActionResult Login()
+    {
+        return View("~/Views/Auth/Login.cshtml");
+    }
+
+    [Route("signup")]
+    public IActionResult SignUp()
+    {
+        return View("~/Views/Auth/SignUp.cshtml");
+    }
+
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
