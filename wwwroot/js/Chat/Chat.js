534ce7349ea8d4059dbb80dbe2166cbfedb5e0d2
Anantha250 <129850658+Anantha250@users.noreply.github.com>
2025-03-04 12:20:57 +0700

    Update Theme

--- wwwroot/js/Chat/Chat.js
+++ wwwroot/js/Chat/Chat.js
@@ -335,15 +335,10 @@ async function loadPreviousMessages() {
     }
 }
 
-function toggleThemeMenu() {
-    let menu = document.querySelector(".theme-dropdown");
-    menu.style.display = menu.style.display === "block" ? "none" : "block";
-}
 
 function toggleThemeMenu() {
     let menu = document.querySelector(".theme-dropdown");
 
-    // เช็คสถานะ ถ้าแสดงอยู่ให้ซ่อน, ถ้าไม่แสดงให้เปิด
     menu.style.display = (menu.style.display === "block") ? "none" : "block";
 }
 
